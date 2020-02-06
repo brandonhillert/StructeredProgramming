8c04a4aedf6f6d2bcffbdda3d2b9c54611ddc451
brandon <brandon.hillert@hva.nl>
2020-02-06 14:14:39 +0100

    Opdracht 1

--- Formatieve Opdracht 1.py
+++ Formatieve Opdracht 1.py
@@ -1,11 +1,82 @@
+"""Opdracht 1 Piramide
+Samen gemaakt samen met Julian van der Geest
+"""
+
 print("Geef een grootte:")
 grootte = int(input())
 
-
 for x in range(grootte):
     x = x + 1
-    print("lol" * x)
+    print("*" * x)
 
 for x in range(grootte):
     grootte = (grootte - 1)
-    print("lol" * grootte)
+    print("*" * grootte)
+
+
+
+
+""""
+Opdracht 1 piramide Do-while Loop"""
+
+print("Geef een grootte:")
+grootte = int(input())
+
+x = 1
+while x < grootte + 1:
+    print('*' * x)
+    x = x + 1
+
+x = 1
+while x < grootte + 1:
+    print((grootte - x) * "*")
+    x = x + 1
+
+"""Opdracht 1 : piramide de andere kant op 
+Samen gemaakt met Julian van Der Geest ( heb het hem uitgelegd )"""
+
+print("Geef een grootte:")
+grootte = int(input())
+
+for x in range(grootte):
+    aantalspaties = str(" " * (grootte - (x + 1)))
+    aantalfiguren = str((x + 1) * "*")
+    print(aantalspaties + aantalfiguren)
+
+for x in range(grootte):
+    aantalspaties = str("*" * (grootte - (x + 1)))
+    aantalfiguren = str((x + 1) * " ")
+    print(aantalfiguren + aantalspaties)
+
+
+
+""""
+Opdracht 1 piramide Do-while Loop"""
+
+print("Geef een grootte:")
+grootte = int(input())
+
+x = 1
+while x < grootte + 1:
+
+    print((" " * (grootte - x)) + ('*' * x))
+    x = x + 1
+
+
+x = 1
+while x < grootte + 1:
+    print((' ' * x) + ("*" * (grootte - x)))
+    x = x + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
