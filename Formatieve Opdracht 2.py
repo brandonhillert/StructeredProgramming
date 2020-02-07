863c1a035e7bd326c949fb2c125af051a54e6fd1
brandon <brandon.hillert@hva.nl>
2020-02-07 14:12:34 +0100

    Opdracht 2

---
+++ Formatieve Opdracht 2.py
@@ -0,0 +1,18 @@
+#def geef_index():
+def zoek_verschillende_waarde():
+
+    print("Geef een string:")
+    string_nummer_1 = str(input())
+
+    print("Geef een string:")
+    string_nummer_2 = str(input())
+
+
+    if string_nummer_1 == string_nummer_2:
+        print("De string zijn precies hetzelfde gelijk")
+    else:
+
+
+
+
+zoek_verschillende_waarde()
