954257f433ee33b6395d75f8e1191a8c89601b69
brandon <brandon.hillert@hva.nl>
2020-02-15 14:50:03 +0100

    Comments toegevoegd

--- Mastering Mastermind/Mastering Mastermind.py
+++ Mastering Mastermind/Mastering Mastermind.py
@@ -1,13 +1,15 @@
 import random
 
+"""Deze functie creeërt een code die voldoet aan de volgende eisen:
+    1. De code bestaat uit 4 getallen
+    2. De code moet een willekeurig getal zijn, die een waarde heeft tussen 0000 en 9999
+"""
 
 def code_genereren():
     code = []
-
     for nummer in range(4):
         code.append(random.randint(0, 9))
     return code
-
 
 
 """Deze functie controleert of de gok van de gebruiker voloet aan de eisen:
