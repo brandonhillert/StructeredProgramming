b0b56eb2e830c87be519dd3d7bf49d71be072282
brandon <brandon.hillert@hva.nl>
2020-02-18 12:29:42 +0100

    Feedback mens functie

--- Mastering Mastermind/mastermind_comp_vs_me.py
+++ Mastering Mastermind/mastermind_comp_vs_me.py
@@ -1,4 +1,5 @@
 import itertools
+import random
 
 """Deze functie retouneert een lijst met alle mogelijke combinaties van abcdef"""
 """https://stackoverflow.com/questions/45990454/generating-all-possible-combinations-of-characters-in-a-string"""
@@ -32,4 +33,55 @@ def random_combinatie_computer(lijst):
     return random.choice(lijst)
 
 
-def feedback():
+"""" Hier zou ik nog een if/else/while/for loop kunnen maken die de input controleert":
+Als zwartpinnen is 0, moet witte pinnen 0 tm 4 zijn
+Als zwartepinnen is 1, moeten witte pinnen 0 tm 3 zijn
+Als zwartepinnen is 2 moeten witte pinnen 0 tm 2 zijn
+Als zwartepinnen is 3, moeten witte pinnen 0 tm 1 zijn
+Als zwartepinnen is 4, moeten wittte pinnen 0 zijn
+"""
+def feedback_geven_mens():
+    feedback = []
+
+    print("Geef aantal zwarten pinnen: ")
+    zwarten_pinnen = int(input())
+
+    print("Geef aantal witte pinnen: ")
+    witte_pinnen = int(input())
+
+    feedback.append(zwarten_pinnen)
+    feedback.append(witte_pinnen)
+
+    return feedback
+
+x = feedback_geven_mens()
+
+
+"""De computer analyseert de feedback die is gegeven, """
+
+def feedback_analyseren_comp(x):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
